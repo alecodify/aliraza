aa836e56266b2ce825f6d758e1b4a7a677159375
aliraza <chaudaryaliraza0@gmail.com>
2025-03-04 17:30:52 +0500

    set button text

--- src/components/MobileAppCard.jsx
+++ src/components/MobileAppCard.jsx
@@ -18,7 +18,7 @@ function MobileAppCard({ imgPath, title, description, apkLink }) {
                 download
                 className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition"
             >
-                Download APK
+                Download
             </a>
         </div>
        
